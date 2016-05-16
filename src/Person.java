e0f4faa2b96ad3d5ba2ff7d2f17c04d8f6b20461
Erik <eschneider1128@gmail.com>
2016-05-16 11:53:07 -0400

    Initial import

---
+++ src/Person.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Erik on 5/16/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+    Person[] family = new Person[10]; // Hard coded to 10 family memebers
+
+}
